c51179108679b4d745e14cf2073eaa075165c729
Oksana-Marchenko11 <125740131+Oksana-Marchenko11@users.noreply.github.com>
2023-07-25 12:22:19 +0000

    Deploying to gh-pages from @ Oksana-Marchenko11/goit-js-hw-09@8a647f205137621966957722882541a9c18ff91f 🚀

---
+++ 01-color-switcher.c6373dbc.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]");console.log(e);const t=document.querySelector("[data-stop]");console.log(t);const o=document.querySelector("body");t.disabled=!0,e.addEventListener("click",(function(){timerId=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(){clearInterval(timerId),e.disabled=!1,t.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.c6373dbc.js.map
