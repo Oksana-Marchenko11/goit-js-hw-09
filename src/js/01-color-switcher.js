8a81623f975a4ff54176ef8f5afe56838e4d9668
Oksana-Marchenko11 <oksana.marchenko1988@gmail.com>
2023-07-26 14:16:43 +0300

    active button stop

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,7 +4,6 @@ const buttonClose = document.querySelector('[data-stop]');
 console.log(buttonClose);
 const body = document.querySelector('body');
 
-
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
