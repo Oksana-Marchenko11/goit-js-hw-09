8f6a106593ba33255bf61c725fd3a6b15f9b05d4
Oksana-Marchenko11 <oksana.marchenko1988@gmail.com>
2023-07-26 17:05:48 +0300

    timerId

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,6 +3,7 @@ console.log(buttonStart);
 const buttonClose = document.querySelector('[data-stop]');
 console.log(buttonClose);
 const body = document.querySelector('body');
+let timerId;
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
